1404c2fbfba77b4b1bda522381b2fe7e928692fb
itorouk <itoroukpong1@gmail.com>
2025-04-30 16:44:46 +0100

    chore: upgrade bdk-jvm to 1.2.0

--- 2.list-unspent.main.kts
+++ 2.list-unspent.main.kts
@@ -1,10 +1,10 @@
 /**
- * bdk-jvm 1.0.0-beta.6
+ * bdk-jvm 1.2.0
  *
  * List the unspent outputs this wallet controls.
  */
 
-@file:DependsOn("org.bitcoindevkit:bdk-jvm:1.0.0-beta.6")
+@file:DependsOn("org.bitcoindevkit:bdk-jvm:1.2.0")
 
 import org.bitcoindevkit.Descriptor
 import org.bitcoindevkit.Wallet
@@ -12,7 +12,7 @@ import org.bitcoindevkit.EsploraClient
 import org.bitcoindevkit.Connection
 import org.bitcoindevkit.FullScanRequest
 import org.bitcoindevkit.LocalOutput
-import org.rustbitcoin.bitcoin.Network
+import org.bitcoindevkit.Network
 
 val SIGNET_ESPLORA_URL = "http://signet.bitcoindevkit.net"
 val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.SIGNET)
@@ -22,6 +22,8 @@ var connection: Connection = Connection.newInMemory()
 val wallet: Wallet = Wallet(descriptor, changeDescriptor, Network.SIGNET, connection)
 val esploraClient: EsploraClient = EsploraClient(SIGNET_ESPLORA_URL)
 val fullScanRequest: FullScanRequest = wallet.startFullScan().build()
+println("Performing full scan on $SIGNET_ESPLORA_URL")
+
 val update = esploraClient.fullScan(fullScanRequest, 10uL, 1uL)
 
 wallet.applyUpdate(update)
